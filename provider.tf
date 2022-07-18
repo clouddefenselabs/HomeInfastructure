5fb67748f54d0681200b7105715b5decd12fd3a8
sigz86 <justin@johnsonbunch.com>
2022-07-18 18:03:51 -0400

    Initial commit

---
+++ provider.tf
@@ -0,0 +1,43 @@
+# Proxmox Provider
+# ---
+# Initial Provider Configuration for Proxmox
+
+terraform {
+
+    required_version = ">= 0.13.0"
+
+    required_providers {
+        proxmox = {
+            source = "telmate/proxmox"
+            version = "2.9.10" # Latest as of 7/18/2022
+        }
+    }
+}
+
+# Variables below defined in credential.auto.tfvars
+variable "proxmox_api_url" {
+    type = string
+}
+
+variable "proxmox_api_token_id" {
+    type = string
+}
+
+variable "proxmox_api_token_secret" {
+    type = string
+}
+
+variable "ubuntu_22_template" {
+    type = string
+}
+
+provider "proxmox" {
+
+    pm_api_url = var.proxmox_api_url
+    pm_api_token_id = var.proxmox_api_token_id
+    pm_api_token_secret = var.proxmox_api_token_secret
+
+    # (Optional) Skip TLS Verification
+    # pm_tls_insecure = true
+
+}
